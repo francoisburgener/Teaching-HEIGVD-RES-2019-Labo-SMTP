87d1a7db4ace4c3b92105fdec26f192792a9bb8f
unknown <batach31@hotmail.com>
2019-04-10 13:15:38 +0200

    smtpclientand prankgenerator

--- SMTP/src/main/java/SMTP/ISmtpClient.java
+++ SMTP/src/main/java/SMTP/ISmtpClient.java
@@ -1,5 +1,9 @@
 package SMTP;
 
+import model.mail.Mail;
+
+import java.io.IOException;
+
 public interface ISmtpClient {
-	
+	void sendMail(Mail mail) throws IOException;
 }
--- SMTP/src/main/java/SMTP/SmtpClient.java
+++ SMTP/src/main/java/SMTP/SmtpClient.java
@@ -1,3 +1,73 @@
 package SMTP;
 
-public class SmtpClient {}
+import model.mail.Mail;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class SmtpClient implements ISmtpClient{
+
+    private String address;
+    private int port;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public SmtpClient(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    private void connect() throws IOException {
+        socket = new Socket(address, port);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    private void disconnect() throws IOException {
+        writer.flush();
+        writer.close();
+        reader.close();
+        socket.close();
+    }
+
+    public void sendMail(Mail mail) throws IOException {
+        connect();
+
+        String line = reader.readLine();
+        writer.println("EHLO ping");
+        writer.flush();
+
+        while ((line = reader.readLine()).equals("250 "));
+
+        writer.println("MAIL FROM: <" + mail.getFrom() + ">");
+        writer.flush();
+
+        line = reader.readLine();
+
+        for(String to : mail.getTo()){
+            writer.println("RCPT TO: <" + to + ">");
+            writer.flush();
+            line = reader.readLine();
+        }
+
+        writer.println("DATA");
+        writer.flush();
+
+        line = reader.readLine();
+
+        writer.println("From: <" + mail.getFrom() + ">");
+        writer.flush();
+
+        writer.print("To: ");
+        for(String to : mail.getTo()){
+            writer.print("<" + to + ">,");
+            writer.flush();
+            line = reader.readLine();
+        }
+
+        disconnect();
+    }
+
+}
